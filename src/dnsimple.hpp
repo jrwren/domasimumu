fc3cc83bdbdc059a37212ce428d68b0d7f7fe705
Jay R. Wren <jrwren@xmtp.net>
2017-11-02 20:56:59 -0400

    use clang-format

--- src/dnsimple.hpp
+++ src/dnsimple.hpp
@@ -1,4 +1,4 @@
-// Copyright © 2014-2015 Jay R. Wren <jrwren@xmtp.net>.
+// Copyright © 2014-2017 Jay R. Wren <jrwren@xmtp.net>.
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
@@ -6,148 +6,143 @@
 #ifndef _DNSIMPLE_HPP_
 #define _DNSIMPLE_HPP_
 
-#include<iostream>
-#include<string>
-#include<json/json.h>
-#include"curlpp11.hpp"
+#include <iostream>
+#include <string>
+#include <json/json.h>
+#include "curlpp11.hpp"
 
-template<typename T, typename ...Args>
-std::unique_ptr<T> make_unique( Args&& ...args )
-{
-    return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+template <typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args &&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 
 namespace dnsimple {
-    class record {
-        public:
-            std::string id;
-            std::string name;
-            std::string record_type;
-            std::string content;
-    };
-    class domain {
-        public:
-            int id;
-            int user_id;
-            int registrant_id;
-            std::string name;
-            std::string unicode_name;
-            std::string token;
-            std::string state;
-            std::string language;
-            bool lockable;
-            bool auto_renew;
-            bool whois_protected;
-            int record_count;
-            int service_count;
-            std::string expires_on;
-            std::string created_at;
-            std::string updated_at;
-    };
-    class change_record {
-        public:
-            std::string name;
-            std::string value;
-            std::string type;
-            std::string ttl;
-    };
-    class client {
-        public:
-        client(std::string email, std::string token) :
-            token{std::move(token)},
-            email{std::move(email)},
-            url{"https://api.dnsimple.com/v1"}
-        {
-        }
-        std::string get(std::string endpoint) {
-            std::string results;
-            curl::Easy c;
-            c.url((url + endpoint).c_str())
-                .header(("X-DNSimple-Token: " + email + ":" + token).c_str())
-                .header("Accept: application/json")
-                .perform(results);
-            return results;
-        }
-        std::vector<domain> get_domains() {
-            std::vector<domain> domains;
-            std::string results = get("/domains");
-//            std::cout << results << std::endl;
+class record {
+public:
+  std::string id;
+  std::string name;
+  std::string record_type;
+  std::string content;
+};
+class domain {
+public:
+  int id;
+  int user_id;
+  int registrant_id;
+  std::string name;
+  std::string unicode_name;
+  std::string token;
+  std::string state;
+  std::string language;
+  bool lockable;
+  bool auto_renew;
+  bool whois_protected;
+  int record_count;
+  int service_count;
+  std::string expires_on;
+  std::string created_at;
+  std::string updated_at;
+};
+class change_record {
+public:
+  std::string name;
+  std::string value;
+  std::string type;
+  std::string ttl;
+};
+class client {
+public:
+  client(std::string email, std::string token)
+      : token{std::move(token)}, email{std::move(email)},
+        url{"https://api.dnsimple.com/v1"} {}
+  std::string get(std::string endpoint) {
+    std::string results;
+    curl::Easy c;
+    c.url((url + endpoint).c_str())
+        .header(("X-DNSimple-Token: " + email + ":" + token).c_str())
+        .header("Accept: application/json")
+        .perform(results);
+    return results;
+  }
+  std::vector<domain> get_domains() {
+    std::vector<domain> domains;
+    std::string results = get("/domains");
+    //            std::cout << results << std::endl;
 
-            Json::Value root;
-            Json::Reader reader;
-            bool parsingSuccessful = reader.parse(results, root);
-            if (!parsingSuccessful) {
-                std::cerr << "Failed to parse response\n"
-                    << reader.getFormattedErrorMessages();
-                return domains;
-            }
-            if (root.isObject()) {
-                std::cerr << "Unexpected response:" << results << std::endl;
-                return domains;
-            }
-            int i = 0;
-/*            for (auto item = root.begin();
-                    item!=root.end();
-                    item++) { */
-            int size = root.size();
-            std:: cout << "root size: " << size << std::endl;
-            for (int index = 0 ; index < size ; ++index) {
-                auto item = root[index]["domain"];
-                auto members = item.getMemberNames();
-/*                std::cout << i++ << ": has name:";
-                std::cout << item.isMember("domain") << " ";
-                std::cout << item["domain"] << " ";
-                for (auto member = members.begin();
-                        member != std::end(members);
-                        member++)
-                    std::cout << *member << ", " ;
-                std::cout << std::endl;*/
-                domain d;
-                d.id = item["id"].asInt();
-                d.user_id = item["user_id"].asInt();
-                d.registrant_id = item["registrant_id"].asInt();
-                d.name = item["name"].asString();
-                d.unicode_name = item["unicode_name"].asString();
-                d.token = item["token"].asString();
-                d.state = item["state"].asString();
-                d.language = item["language"].asString();
-                d.lockable = item["lockable"].asBool();
-                d.auto_renew = item["auto_renew"].asBool();
-                d.whois_protected = item["whois_protected"].asBool();
-                d.record_count = item["record_count"].asInt();
-                d.service_count = item["service_count"].asInt();
-                d.expires_on = item["expires_on"].asString();
-                d.created_at = item["created_at"].asString();
-                d.updated_at = item["updated_at"].asString();
-                d.id = item["id"].asInt();
-                domains.push_back(d);
-            }
+    Json::Value root;
+    Json::Reader reader;
+    bool parsingSuccessful = reader.parse(results, root);
+    if (!parsingSuccessful) {
+      std::cerr << "Failed to parse response\n"
+                << reader.getFormattedErrorMessages();
+      return domains;
+    }
+    if (root.isObject()) {
+      std::cerr << "Unexpected response:" << results << std::endl;
+      return domains;
+    }
+    int i = 0;
+    /*            for (auto item = root.begin();
+                        item!=root.end();
+                        item++) { */
+    int size = root.size();
+    std::cout << "root size: " << size << std::endl;
+    for (int index = 0; index < size; ++index) {
+      auto item = root[index]["domain"];
+      auto members = item.getMemberNames();
+      /*                std::cout << i++ << ": has name:";
+                      std::cout << item.isMember("domain") << " ";
+                      std::cout << item["domain"] << " ";
+                      for (auto member = members.begin();
+                              member != std::end(members);
+                              member++)
+                          std::cout << *member << ", " ;
+                      std::cout << std::endl;*/
+      domain d;
+      d.id = item["id"].asInt();
+      d.user_id = item["user_id"].asInt();
+      d.registrant_id = item["registrant_id"].asInt();
+      d.name = item["name"].asString();
+      d.unicode_name = item["unicode_name"].asString();
+      d.token = item["token"].asString();
+      d.state = item["state"].asString();
+      d.language = item["language"].asString();
+      d.lockable = item["lockable"].asBool();
+      d.auto_renew = item["auto_renew"].asBool();
+      d.whois_protected = item["whois_protected"].asBool();
+      d.record_count = item["record_count"].asInt();
+      d.service_count = item["service_count"].asInt();
+      d.expires_on = item["expires_on"].asString();
+      d.created_at = item["created_at"].asString();
+      d.updated_at = item["updated_at"].asString();
+      d.id = item["id"].asInt();
+      domains.push_back(d);
+    }
 
-            return domains;
-        }
-        std::tuple<std::vector<record>, int> get_records(std::string domain) {
-            std::vector<record> records;
-            std::ostringstream os;
-            //os << cURLpp::Options::Url(url + "/domains/" + domain + "/records");
-            std::cout << os.str();
-            return std::make_tuple(records, 0);
-        }
-        std::tuple<std::string, int> create_record(std::string domain, change_record *rchange) {
-            std::string id;
-            return std::make_tuple(id, 0);
-        }
-        std::tuple<std::string, int> update_record(std::string domain, change_record *change) {
-            std::string id;
-            return std::make_tuple(id, 0);
-        }
-        int destroy_record(std::string domain, std::string id) {
-            return 0;
-        }
-        std::string token;
-        std::string email;
-        std::string url;
-
-    };
+    return domains;
+  }
+  std::tuple<std::vector<record>, int> get_records(std::string domain) {
+    std::vector<record> records;
+    std::ostringstream os;
+    // os << cURLpp::Options::Url(url + "/domains/" + domain + "/records");
+    std::cout << os.str();
+    return std::make_tuple(records, 0);
+  }
+  std::tuple<std::string, int> create_record(std::string domain,
+                                             change_record *rchange) {
+    std::string id;
+    return std::make_tuple(id, 0);
+  }
+  std::tuple<std::string, int> update_record(std::string domain,
+                                             change_record *change) {
+    std::string id;
+    return std::make_tuple(id, 0);
+  }
+  int destroy_record(std::string domain, std::string id) { return 0; }
+  std::string token;
+  std::string email;
+  std::string url;
+};
 }
 
 #endif
--- src/domasimumu.cpp
+++ src/domasimumu.cpp
@@ -1,222 +1,219 @@
-// Copyright © 2014-2015 Jay R. Wren <jrwren@xmtp.net>.
+// Copyright © 2014-2017 Jay R. Wren <jrwren@xmtp.net>.
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-#include<string>
-#include<sstream>
-#include<iostream>
-#include<boost/program_options.hpp>
-#include"curlpp11.hpp"
-#include"cpptoml.h"
-#include<cstdlib>
-#include<boost/filesystem.hpp>
-#include"dnsimple.hpp"
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <boost/program_options.hpp>
+#include "curlpp11.hpp"
+#include "cpptoml.h"
+#include <cstdlib>
+#include <boost/filesystem.hpp>
+#include "dnsimple.hpp"
 
 using namespace std;
 
 class config {
-    public:
-    string user;
-    string token;
+public:
+  string user;
+  string token;
 };
 
 int decode_config(cpptoml::toml_group g, config *c) {
-    if (g.contains("user"))
-        //c->user = g.get("user")->as<std::string>()->value();
-        c->user = *g.get_as<string>("user");
-    else
-        return 2;
-    if (g.contains("token"))
-        //c->token = g.get("token")->as<std::string>()->value();
-        c->token = *g.get_as<string>("token");
-    else
-        return 3;
-   return 0;
+  if (g.contains("user"))
+    // c->user = g.get("user")->as<std::string>()->value();
+    c->user = *g.get_as<string>("user");
+  else
+    return 2;
+  if (g.contains("token"))
+    // c->token = g.get("token")->as<std::string>()->value();
+    c->token = *g.get_as<string>("token");
+  else
+    return 3;
+  return 0;
 }
 
 int get_creds(string *user, string *token) {
-    char* conf_env = getenv("DOMASIMU_CONF");
-    string configFileName;
-    if (conf_env != NULL) {
-        configFileName = conf_env;
-    }
-    if (configFileName=="") {
-        configFileName =
-            boost::filesystem::absolute(
-                    ".domasimurc",
-                    getenv("HOME")
-                    ).string();
-    }
-    cpptoml::toml_group g;
-    try
-    {
-        g = cpptoml::parse_file(configFileName);
-    }
-    catch (const cpptoml::toml_parse_exception& e)
-    {
-        std::cerr << "Failed to parse " << configFileName << ": " << e.what() << std::endl;
-        return 1;
-    }
-    config c;
-    int err = decode_config(g, &c);
-    if (err != 0) {
-        return err;
-    }
-    *user = c.user;
-    *token = c.token;
-    return 0;
+  char *conf_env = getenv("DOMASIMU_CONF");
+  string configFileName;
+  if (conf_env != NULL) {
+    configFileName = conf_env;
+  }
+  if (configFileName == "") {
+    configFileName =
+        boost::filesystem::absolute(".domasimurc", getenv("HOME")).string();
+  }
+  cpptoml::toml_group g;
+  try {
+    g = cpptoml::parse_file(configFileName);
+  } catch (const cpptoml::toml_parse_exception &e) {
+    std::cerr << "Failed to parse " << configFileName << ": " << e.what()
+              << std::endl;
+    return 1;
+  }
+  config c;
+  int err = decode_config(g, &c);
+  if (err != 0) {
+    return err;
+  }
+  *user = c.user;
+  *token = c.token;
+  return 0;
 }
 
-tuple<string, int> get_record_id_by_value(unique_ptr<dnsimple::client> const &client, string domain, dnsimple::change_record *change_record) {
-    auto resp = client->get_records(domain);
-    auto records = get<0>(resp);
-    auto err = get<1>(resp);
+tuple<string, int>
+get_record_id_by_value(dnsimple::client &client, string domain,
+                       dnsimple::change_record *change_record) {
+  auto resp = client->get_records(domain);
+  auto records = get<0>(resp);
+  auto err = get<1>(resp);
+  string id;
+  if (err != 0) {
+    return tuple<string, int>(id, err);
+  }
+  for (auto record = records.begin(); record != records.end(); record++) {
+    if (record->name == change_record->name &&
+        record->record_type == change_record->type &&
+        record->content == change_record->value) {
+      id = record->id;
+      break;
+    }
+  }
+  return make_tuple(id, 0);
+}
+
+tuple<string, int> create_or_update(unique_ptr<dnsimple::client> const &client,
+                                    string message) {
+  string domain;
+  istringstream msg_stream(message);
+  dnsimple::change_record change_record;
+  msg_stream >> domain >> change_record.name >> change_record.type >>
+      change_record.value;
+  dnsimple::change_record new_record = change_record;
+  msg_stream >> new_record.value >> new_record.ttl;
+  string id;
+  int err;
+  tie(id, err) = get_record_id_by_value(*client, domain, &change_record);
+  if (err != 0) {
+    return tuple<string, int>(0, err);
+  }
+  string resp_id;
+  if (id.empty()) {
+    tie(resp_id, err) = client->create_record(domain, &new_record);
+  } else {
+    tie(resp_id, err) = client->update_record(domain, &new_record);
+  }
+  if (err != 0) {
+    return tuple<string, int>(0, err);
+  }
+  return tuple<string, int>(resp_id, 0);
+}
+
+tuple<string, int> delete_record(unique_ptr<dnsimple::client> const &client,
+                                 string message) {
+  string domain;
+  istringstream msg_stream(message);
+  dnsimple::change_record change_record;
+  msg_stream >> domain >> change_record.name >> change_record.type >>
+      change_record.value;
+  string id;
+  int err;
+  tie(id, err) = get_record_id_by_value(*client, domain, &change_record);
+  if (err != 0) {
+    return tuple<string, int>(id, err);
+  }
+  if (id.empty()) {
+    return make_tuple(id, 4);
+  }
+  err = client->destroy_record(domain, id);
+  return tuple<string, int>(id, 0);
+}
+
+int main(int argc, char **argv) {
+  curl::GlobalSentry curl;
+  namespace po = boost::program_options;
+  po::options_description desc("Options");
+  desc.add_options()("help,h", "Print help message")(
+      "verbose,v", "Use verbose output")("list,l", "List domains")(
+      "update,u", po::value<string>(), "Update or create record")(
+      "delete,d", po::value<string>(),
+      "Delete record")("domain", po::value<vector<string>>(), "domain");
+
+  po::positional_options_description p;
+  p.add("domain", -1);
+
+  po::variables_map vm;
+
+  // po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::store(
+      po::command_line_parser(argc, argv).options(desc).positional(p).run(),
+      vm);
+  notify(vm);
+
+  if (vm.count("help")) {
+    cout << desc << ".domasimumu config file example:" << endl;
+    return 0;
+  }
+
+  bool verbose = false;
+  if (vm.count("verbose")) {
+    verbose = true;
+  }
+
+  string user, token;
+  int err = get_creds(&user, &token);
+  if (err != 0) {
+    cerr << "could not read config:" << err << endl;
+    return err;
+  }
+
+  // auto client = make_unique( dnsimple::client(user, token) );
+  // dnsimple::client *client = new dnsimple::client(user, token) ;
+  std::unique_ptr<dnsimple::client> client(new dnsimple::client(user, token));
+
+  if (vm.count("list")) {
+    auto domains = client->get_domains();
+    /*        for(auto domain = domains.begin();
+                    domain != domains.end();
+                    domain++) {*/
+    for (auto domain : domains) {
+      if (verbose) {
+        cout << domain.name << " " << domain.expires_on << endl;
+      } else {
+        cout << domain.name << endl;
+      }
+    }
+  }
+  if (vm.count("update")) {
     string id;
-    if (err != 0) {
-        return tuple<string, int>(id,err);
-    }
-    for (auto record = records.begin();
-            record != records.end();
-            record++) {
-        if (record->name == change_record->name &&
-            record->record_type == change_record->type &&
-            record->content == change_record->value) {
-            id = record->id;
-            break;
-        }
-    }
-    return make_tuple(id, 0);
-}
-
-tuple<string, int> create_or_update(unique_ptr<dnsimple::client> const &client, string message) {
-    string domain;
-    istringstream msg_stream(message);
-    dnsimple::change_record change_record;
-    msg_stream >> domain >> change_record.name >> change_record.type >>
-        change_record.value ;
-    dnsimple::change_record new_record = change_record;
-    msg_stream >> new_record.value >> new_record.ttl;
-    string id; int err;
-    tie(id, err) = get_record_id_by_value(client, domain, &change_record);
-    if (err != 0) {
-        return tuple<string, int>(0,err);
-    }
-    string resp_id;
-    if (id.empty()) {
-        tie(resp_id, err) = client->create_record(domain, &new_record);
+    int err;
+    tie(id, err) = create_or_update(client, vm["update"].as<string>());
+    if (err) {
+      cerr << "could not create or update:" << err << endl;
     } else {
-        tie(resp_id, err) = client->update_record(domain, &new_record);
+      cout << "record written with id " << id << endl;
     }
-    if (err != 0) {
-        return tuple<string, int>(0, err);
+  }
+  if (vm.count("delete")) {
+    string id;
+    int err;
+    tie(id, err) = delete_record(client, vm["delete"].as<string>());
+    if (err) {
+      cerr << "could not delete:" << err << endl;
+    } else {
+      cout << "record deleted with id " << id << endl;
     }
-    return tuple<string, int>(resp_id, 0);
-}
-
-tuple<string, int> delete_record(unique_ptr<dnsimple::client> const &client, string message) {
-    string domain;
-    istringstream msg_stream(message);
-    dnsimple::change_record change_record;
-    msg_stream >> domain >> change_record.name >> change_record.type >>
-        change_record.value ;
-    string id; int err;
-    tie(id, err) = get_record_id_by_value(client, domain, &change_record);
-    if (err != 0) {
-        return tuple<string, int>(id,err);
+  }
+  if (vm.count("domain")) {
+    for (auto domain = vm["domain"].as<vector<string>>().begin();
+         domain != vm["domain"].as<vector<string>>().end(); domain++) {
+      vector<dnsimple::record> records;
+      tie(records, err) = client->get_records(*domain);
+      cout << *domain << endl;
+      //            cout << records << endl;
     }
-    if (id.empty()) {
-        return make_tuple(id, 4);
-    }
-    err = client->destroy_record(domain, id);
-    return tuple<string, int>(id, 0);
-}
-
-int main(int argc, char** argv) {
-    curl::GlobalSentry curl;
-    namespace po = boost::program_options;
-    po::options_description desc("Options");
-    desc.add_options()
-        ("help,h", "Print help message")
-        ("verbose,v", "Use verbose output")
-        ("list,l", "List domains")
-        ("update,u", po::value<string>(), "Update or create record")
-        ("delete,d", po::value<string>(), "Delete record")
-        ("domain", po::value< vector<string> >(), "domain")
-        ;
-
-    po::positional_options_description p;
-    p.add("domain", -1);
-
-    po::variables_map vm;
-
-    //po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
-    notify(vm);
-
-    if (vm.count("help")) {
-        cout << desc <<
-            ".domasimumu config file example:" <<
-            endl;
-        return 0;
-    }
-
-    bool verbose = false;
-    if (vm.count("verbose")) {
-        verbose = true;
-    }
-
-    string user, token;
-    int err = get_creds(&user, &token);
-    if (err != 0) {
-        cerr << "could not read config:" << err << endl;
-        return err;
-    }
-
-    //auto client = make_unique( dnsimple::client(user, token) );
-    //dnsimple::client *client = new dnsimple::client(user, token) ;
-    std::unique_ptr<dnsimple::client> client(new dnsimple::client(user, token));
-
-    if (vm.count("list")) {
-        auto domains = client->get_domains();
-/*        for(auto domain = domains.begin();
-                domain != domains.end();
-                domain++) {*/
-        for (auto domain : domains) {
-            if (verbose) {
-                cout << domain.name << " " << domain.expires_on << endl;
-            } else {
-                cout << domain.name << endl;
-            }
-        }
-    }
-    if (vm.count("update")) {
-        string id; int err;
-        tie(id, err) = create_or_update(client, vm["update"].as<string>());
-        if (err) {
-            cerr << "could not create or update:" << err << endl;
-        } else {
-            cout << "record written with id " << id << endl;
-        }
-    }
-    if (vm.count("delete")) {
-        string id; int err;
-        tie(id, err) = delete_record(client, vm["delete"].as<string>());
-        if (err) {
-            cerr << "could not delete:" << err << endl;
-        } else {
-            cout << "record deleted with id " << id << endl;
-        }
-    }
-    if (vm.count("domain")) {
-        for(auto domain = vm["domain"].as< vector<string> >().begin();
-                domain != vm["domain"].as< vector<string> >().end();
-                domain ++) {
-            vector<dnsimple::record> records;
-            tie(records,err) = client->get_records(*domain);
-            cout << *domain << endl;
-//            cout << records << endl;
-        }
-    }
-    return 0;
+  }
+  return 0;
 }
